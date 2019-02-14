724a501e7e186abbcfcd5a7ddd995fcac30287d4
Harouna Traore <h.traore@icloud.com>
2019-02-14 08:59:58 +0100

    rating component to page

---
+++ how-to-unit-test-your-first-vue-js-component/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <rating/>
+  </div>
+</template>
+
+<script>
+import rating from './rating/rating.vue'
+
+export default {
+  name: 'app',
+  components: {
+    rating
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- how-to-unit-test-your-first-vue-js-component/src/rating/rating.vue
+++ how-to-unit-test-your-first-vue-js-component/src/rating/rating.vue
@@ -4,6 +4,8 @@
 
 <script>
 export default {
+  name: 'rating',
+  props: {}
 }
 </script>
 
